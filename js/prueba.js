b16de283ae96c4a2c0efef323d526192c3fc34d7
Emilianosanchezlopez <emilianosanchezlopez@hotmail.com>
2017-03-10 17:09:30 -0600

    Add files via upload

---
+++ js/prueba.js
@@ -0,0 +1,42 @@
+myFunction([
+ {
+   "Name": "Diana Torres",
+   "Title": "Consultor Jr",
+   "BusinessPhone": "5599-0000",
+   "Location": "MBM",
+   "Department": "Subdirección de Soluciones Tecnológicas",
+   "Company": "Mainbit"
+ },
+ {
+   "Name": "German Rodríguez",
+   "Title": "Consultor Mr",
+   "BusinessPhone": "5599-0001",
+   "Location": "MBI",
+   "Department": "Subdirección de Procesos",
+   "Company": "Mainbit"
+ },
+ {
+   "Name": "César Godínez",
+   "Title": "Director ",
+   "BusinessPhone": "5599-0002",
+   "Location": "MBM",
+   "Department": "Dirección de Gobernabilidad",
+   "Company": "Mainbit"
+ },
+ {
+   "Name": "Carla Gómez",
+   "Title": "Subdirector",
+   "BusinessPhone": "5599-0003",
+   "Location": "MBI",
+   "Department": "Dirección de Arquitectura",
+   "Company": "Mainbit"
+ },
+ {
+   "Name": "Elias Navarro",
+   "Title": "Gerente ",
+   "BusinessPhone": "5599-0004",
+   "Location": "MBR",
+   "Department": "Subdirección de Calidad",
+   "Company": "Mainbit"
+ }
+])
